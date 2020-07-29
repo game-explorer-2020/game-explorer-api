449d1915c35e6880ce15f7c105072cefb124a055
Junior <vilmarmendesjunior@hotmail.com>
2020-07-29 20:51:18 -0300

    chore: setup mongodb and mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,9 @@
 import app from './app';
+import mongoose from 'mongoose';
 
 app.listen(process.env.PORT || 3333);
+
+mongoose.connect(process.env.MONGODB_URI as string, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
