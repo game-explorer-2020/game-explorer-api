871d7c4e1c252ffaa89ae7790913bc6fde7bcb3a
Junior <vilmarmendesjunior@hotmail.com>
2020-07-30 18:41:42 -0300

    style: remove comments

--- src/routes.ts
+++ src/routes.ts
@@ -10,15 +10,12 @@ const gameController = new GameController();
 const gameDetailsController = new GameDetailsController();
 const feedController = new FeedController();
 
-// Games
 routes.get('/api/v1/games', gameController.index.bind(gameController));
 routes.get('/api/v1/games/favorites', gameController.getFavorites.bind(gameController));
 routes.put('/api/v1/games/favorites/:id', gameController.toggleFavorite.bind(gameController));
 
-// Game details
 routes.get('/api/v1/games/details/:id', gameDetailsController.get.bind(gameDetailsController));
 
-// Feeds
 routes.get('/api/v1/feeds', feedController.index.bind(feedController));
 routes.get('/api/v1/feeds/favorites', feedController.getFavorites.bind(feedController));
 routes.put('/api/v1/feeds/favorites/:id', feedController.toggleFavorite.bind(feedController));
