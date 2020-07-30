27dabba2590afaca0ee2fb86f85145d7726d11ab
Junior <vilmarmendesjunior@hotmail.com>
2020-07-29 22:43:21 -0300

    feat: add favorites listing

--- src/controllers/FavoriteFeedController.ts
+++ src/controllers/FavoriteFeedController.ts
@@ -1,9 +1,33 @@
 import { Request, Response } from 'express';
 
 import FeedSchema from '../schemas/FeedSchema';
+import QueryBuilder from '../lib/QueryBuilder';
+
+import { PulseIGDB } from './../models/igdb/PulseIGDB';
+import { Feed } from './../models/Feed';
+import FeedMapper from '../mappers/FeedMapper';
+import igdbApi from '../configs/igdb-api';
 
 class FavoriteFeedController {
-  async index(request: Request, response: Response) {}
+  async index(request: Request, response: Response) {
+    const { offset = 0 } = request.query;
+    const favoriteFeeds = await FeedSchema.find();
+    const query = new QueryBuilder()
+      .select('title', 'published_at', 'website.url', 'image')
+      .where(`id = (${favoriteFeeds.map(f => f.id)})`)
+      .sort('published_at')
+      .offset(Number(offset))
+      .build();
+
+    try {
+      const igdbApiResponse = await igdbApi.post<PulseIGDB[]>('/pulses', query);
+      const feeds: Feed[] = igdbApiResponse.data.map(pulse => ({ ...FeedMapper.from(pulse), favorite: true }));
+
+      return response.json(feeds);
+    } catch (error) {
+      return response.status(error.response.status).send(error.response.data);
+    }
+  }
 
   async store(request: Request, response: Response) {
     const { id } = request.params;
--- src/controllers/FavoriteGameController.ts
+++ src/controllers/FavoriteGameController.ts
@@ -1,9 +1,32 @@
 import { Request, Response } from 'express';
 
 import GameSchema from '../schemas/GameSchema';
+import QueryBuilder from '../lib/QueryBuilder';
+import igdbApi from '../configs/igdb-api';
+import { GameIGDB } from '../models/igdb/GameIGDB';
+import { Game } from '../models/Game';
+import GameMapper from '../mappers/GameMapper';
 
 class FavoriteGameController {
-  async index(request: Request, response: Response) {}
+  async index(request: Request, response: Response) {
+    const { offset = 0 } = request.query;
+    const favoriteGames = await GameSchema.find();
+    const query = new QueryBuilder()
+      .select('name', 'genres.name', 'platforms.name', 'cover.url')
+      .where(`id = (${favoriteGames.map(g => g.id)})`)
+      .offset(Number(offset))
+      .sort('popularity')
+      .build();
+
+    try {
+      const igdbApiResponse = await igdbApi.post<GameIGDB[]>('/games', query);
+      const games: Game[] = igdbApiResponse.data.map(game => ({ ...GameMapper.from(game), favorite: true }));
+
+      return response.json(games);
+    } catch (error) {
+      return response.status(error.response.status).send(error.response.data);
+    }
+  }
 
   async store(request: Request, response: Response) {
     const { id } = request.params;
